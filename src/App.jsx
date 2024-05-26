d31490d6eb6b469a503d23b3c2ba0dbdf45a279b
abdul <csbadulgaffar@gmail.com>
2024-05-26 23:38:14 +0530

    'videoadded'

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import { BrowserRouter, Routes, Route, } from "react-router-dom"
 import Home from "./pages/Home"
 import AboutUs from "./pages/About"
 import Learn from "./pages/Learn"
+import LearnHouse from "./pages/LearnHouse"
+import LearnNeighbour from "./pages/LearnNeighbour"
+import LearnNutrition from "./pages/LearnNutrition"
+
 
 import Layout from "./component/Layout"
+import LearnBioOne from "./pages/LearnBioOne"
+import LearnBioTwo from "./pages/LearnBioTwo"
 export default function App() {
   return (
     <BrowserRouter>
@@ -14,6 +20,11 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/learn" element={<Learn />} />
+          <Route path="/learn-house" element={<LearnHouse />} />
+          <Route path="/learn-neighbour" element={<LearnNeighbour />} />
+          <Route path="/learn-nutrition" element={<LearnNutrition />} />
+          <Route path="/learn-bio1" element={<LearnBioOne />} />
+          <Route path="/learn-bio2" element={<LearnBioTwo />} />
           <Route path="/learn/:id" element={<Learn />} />
         </Route>
 
---
+++ src/pages/LearnNeighbour.jsx
@@ -0,0 +1,134 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import YouTube from 'react-youtube';
+
+
+import lesson from "../assets/neighbour.mp4"
+const VideoPlayer = ({ src, width, height }) => {
+    return (
+        <div>
+            <video width={width} height={height} controls>
+                <source src={src} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    );
+};
+
+function LearnNeighbour() {
+    const [videoId, setVideoId] = useState('');
+    const [videoLink, setVideoLink] = useState('');
+    const [offline, setOffline] = useState(true);
+    const { id } = useParams();
+    const [opts, setOpts] = useState({
+        height: '720',
+        width: '1280',
+        playerVars: {
+            autoplay: 1,
+        },
+    });
+
+    useEffect(() => {
+        if (id) {
+            setVideoId(id);
+        }
+
+        // Set the initial size of the video player
+        handleScreenResize();
+
+        // Add an event listener to update the video player size on window resize
+        window.addEventListener('resize', handleScreenResize);
+
+        // Clean up the event listener when the component is unmounted
+        return () => {
+            window.removeEventListener('resize', handleScreenResize);
+        };
+    }, [id]);
+
+    const handleScreenResize = () => {
+        const screenWidth = window.innerWidth;
+        let height, width;
+
+        if (screenWidth <= 768) {
+            // Mobile
+            height = '300';
+            width = '390';
+        } else if (screenWidth <= 968) {
+            // Tablet
+            height = '480';
+            width = '854';
+        } else {
+            // Desktop
+            height = '720';
+            width = '1280';
+        }
+
+        setOpts({
+            height: height,
+            width: width,
+            playerVars: {
+                autoplay: 1,
+            },
+        });
+    };
+
+    const onReady = (event) => {
+        event.target.pauseVideo();
+    };
+
+    const onStateChange = (event) => {
+        console.log(event.data);
+    };
+
+    const handleInputChange = (event) => {
+        setVideoLink(event.target.value);
+    };
+
+    const handlePlayVideo = () => {
+        const regex = /(?:https?:\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w-]{11})/;
+        const match = videoLink.match(regex);
+        if (match) {
+            setVideoId(match[1]);
+        } else {
+            alert('Invalid YouTube video URL');
+        }
+    };
+
+    return (
+        <div className="max-w-screen-xl mx-auto">
+            {!offline && videoId && <YouTube
+                videoId={videoId}
+                opts={opts}
+                onReady={onReady}
+                onStateChange={onStateChange}
+            />}
+            {
+                offline && <VideoPlayer src={lesson} width={opts.width} height={opts.height} />
+            }
+
+            {!offline && !videoId &&
+                <>
+                    <div className="max-w-screen-lg mx-auto px-4">
+                        <div className="mb-4">
+                            <input
+                                type="text"
+                                placeholder="Enter YouTube video link or ID"
+                                className="border border-gray-300 px-4 py-2 rounded-md w-full"
+                                value={videoLink}
+                                onChange={handleInputChange}
+                            />
+                            <button
+                                onClick={handlePlayVideo}
+                                className="mt-2 bg-gray-800 hover:scale-105 transition-all duration-300 text-white px-4 py-2 rounded-md"
+                            >
+                                Play Video
+                            </button>
+                        </div>
+                    </div>
+                </>
+            }
+        </div>
+    );
+}
+
+export default LearnNeighbour;
